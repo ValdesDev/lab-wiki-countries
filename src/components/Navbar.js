3776943c19662b86b0b41e8da2fb66e91a0009d4
JoseLValdes <jolvaldgo@gmail.com>
2021-12-03 10:26:31 +0100

    finish

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import { Routes, Route, Link } from "react-router-dom";
+const Navbar =() =>{
+    return(
+        <nav class="navbar navbar-dark bg-primary mb-3">
+        <div class="container">
+          {/* <Link class="navbar-brand" href="/">WikiCountries</Link> */}
+   
+          </div>
+      </nav>
+    )
+}
+
+export default Navbar;
